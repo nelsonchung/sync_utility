bc7f350c6d17f2248f93c7db9c8dcebed44174b9
nelson <nelson@debian>
2015-12-11 16:10:08 +0800

    方便使用者git clone後，可以直接以當下目錄為路徑加入。

--- add_path.sh
+++ add_path.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-#請使用source add_path.sh path1的方式來執行，path才會立即生效
-export PATH="${PATH}:$1"
-echo "export PATH=\"\${PATH}:$1\"" >> ~/.bashrc
+#請使用source add_path.sh的方式來執行，path才會立即生效
+#Now always add current path
+PWD=`pwd`
+export PATH="${PATH}:$PWD"
+echo "export PATH=\"\${PATH}:$PWD\"" >> ~/.bashrc
