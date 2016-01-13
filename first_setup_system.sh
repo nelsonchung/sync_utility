30ac1bd1c04c012395f4cb70542b3a817b6293b0
nelsonchung <chihchun.chung@gmail.com>
2016-01-14 00:22:35 +0800

    add vim install.

--- first_setup_system.sh
+++ first_setup_system.sh
@@ -1,3 +1,4 @@
+sudo apt-get install vim
 sudo apt-get install gperf bison build-essential flex texinfo gawk ncurses*
 sudo apt-get install sed bash dpkg-dev bison flex patch texinfo automake m4 libtool cvs websvn tar gzip bzip2 lzma l              ibncurses5-dev bison flex texinfo automake libtool patch cvs cvsd gawk -y help2man libtool-bin
 sudo apt-get install sed bash dpkg-dev bison flex patch texinfo automake m4 libtool cvs websvn tar gzip bzip2 lzma l              ibncurses5-dev bison flex texinfo automake libtool patch cvs cvsd gawk -y help2man libtool
