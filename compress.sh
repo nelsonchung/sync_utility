3a0fd696c588b52e4fc74d0a24f3721592fd52fa
yayanglu <coolboy6416@gmail.com>
2016-02-24 17:03:44 +0800

    compress.sh

--- compress.sh
+++ compress.sh
@@ -1,9 +1,44 @@
 echo "0. Compress sample cmd"
+echo "11. .tar.Z"
+echo "12. .tgz"
 read option
 
 case "$option" in
     "0")
     echo "Compress sample"
+    ;;
+    "11")
+        echo "Select action."
+        echo "1. Compress file"
+        echo "2. Uncompress file"
+        read action
+        case "$action" in
+        "1")
+            read -p "Please input filename : " filename
+            read -p "Please input dirname : " dirname
+            echo `tar zcvf $filename.tar.Z $dirname`
+            ;;
+        "2")
+            read -p "Please input filename : " filename
+            echo `tar zxvf $filename.tar.Z`
+            ;;
+        esac
+    ;;
+    "12")
+        echo "Select action."
+        echo "1. Compress file"
+        echo "2. Uncompress file"
+        read action
+        case "$action" in
+        "1")
+            read -p "Please input filename : " filename
+            echo `tar zcvf $filename.tgz $filename`
+            ;;
+        "2")
+            read -p "Please input filename : " filename
+            echo `tar zxvf $filename.tgz`
+            ;;
+        esac
     ;;
     "*")
     echo "Not supported"
