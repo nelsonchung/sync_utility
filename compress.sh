840d95d9a576b86c26da72d5ba9aeff0cf901d5d
bruce <bruce9812@gmail.com>
2016-03-02 17:51:40 +0800

    Add compress.sh support ".tar.bz" and "bz2"

--- compress.sh
+++ compress.sh
@@ -1,6 +1,8 @@
 echo "0. Compress sample cmd"
 echo "11. .tar.Z"
 echo "12. .tgz"
+echo "15. .tar.bz"
+echo "16. .bz"
 read option
 
 case "$option" in
@@ -40,6 +42,36 @@ case "$option" in
             ;;
         esac
     ;;
+
+    "15")
+        echo "Select action."
+        echo "1. Uncompress file"
+        read action
+        case "$action" in
+        "1")
+            read -p "Please input filename : " filename
+            echo `tar jxvf $filename`
+            ;;
+        esac
+    ;;
+
+    "16")
+        echo "Select action."
+        echo "1. Compress file"
+        echo "2. Uncompress file"
+        read action
+        case "$action" in
+        "1")
+            read -p "Please input filename : " filename
+            echo `bzip2 -z $filename`
+            ;;
+        "2")
+            read -p "Please input filename : " filename
+            echo `bzip2 -d $filename.bz2`
+            ;;
+        esac
+    ;;
+
     "*")
     echo "Not supported"
     ;;
