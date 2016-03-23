e0d97fd7c454e7a9903af4c88b6cea5778a1f283
nelsonchung <chihchun.chung@gmail.com>
2016-03-23 15:22:47 +0800

    Add two commands. 2. 新增使用者 3. 將使用者擁有sudo權限

--- ubuntu_cmd.sh
+++ ubuntu_cmd.sh
@@ -1,10 +1,21 @@
 
 echo "1. 編輯sources.list"
+echo "2. 新增使用者"
+echo "3. 將使用者擁有sudo權限"
 read option
 
 case "$option" in
     "1")
     sudo vim /etc/apt/sources.list 
+    ;;
+    "2")
+    echo "Input the username"
+    read username
+    sudo adduser $username
+    ;;
+    "3")
+    sudo vim /etc/sudoers
+    sudo cat /etc/sudoers
     ;;
     *)
     ;;
