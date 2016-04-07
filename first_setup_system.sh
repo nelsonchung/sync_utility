b4f652631126ed275be3c7f70062627463d31797
nelsonchung <chihchun.chung@gmail.com>
2016-04-07 14:37:11 +0800

    Update first_setup_system.sh

--- first_setup_system.sh
+++ first_setup_system.sh
@@ -1,6 +1,7 @@
 sudo apt-get install vim
 sudo apt-get install gperf bison build-essential flex texinfo gawk ncurses*
-sudo apt-get install sed bash dpkg-dev bison flex patch texinfo automake m4 libtool cvs websvn tar gzip bzip2 lzma l              ibncurses5-dev bison flex texinfo automake libtool patch cvs cvsd gawk -y help2man libtool-bin
-sudo apt-get install sed bash dpkg-dev bison flex patch texinfo automake m4 libtool cvs websvn tar gzip bzip2 lzma l              ibncurses5-dev bison flex texinfo automake libtool patch cvs cvsd gawk -y help2man libtool
+sudo apt-get install sed bash dpkg-dev bison flex patch texinfo automake m4 libtool cvs websvn tar gzip bzip2 lzma libncurses5-dev bison flex texinfo automake libtool patch cvs cvsd gawk help2man libtool
 sudo apt-get install libexpat1-dev
 sudo apt-get install libperl-dev
+sudo apt-get install tofrodos dos2unix
+sudo apt-get install git
