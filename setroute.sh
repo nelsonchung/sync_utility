d0a4d5988e3a8e1b88f16ee372a07e243739cd98
nelsonchung <chihchun.chung@gmail.com>
2016-06-30 10:33:48 +0800

    1. 修改成選單模式 2. 設定lab網路，透過wlan0出去 3. 設定office網路，透過wlan0出去

--- setroute.sh
+++ setroute.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-sudo route add -net 172.16.0.0 netmask 255.255.0.0 gw 172.16.1.1
+echo "1. for lab netowork via wlan0 (172.16.x.x)"
+echo "2. for office network via wlan0 (10.118.x.x)"
+
+read option
+
+case "$option" in
+    "1")
+        #for lab netowork via wlan0 (172.16.x.x)
+        sudo route add -net 172.16.0.0 netmask 255.255.0.0 gw 172.16.1.1
+    ;;
+    "2")
+        #for office network via wlan0 (10.118.x.x)
+        sudo route add -net 10.118.0.0 netmask 255.255.0.0 dev wlan0
+    ;;
+    *)
+    echo "Not support"
+    exit 1
+    ;;
+esac
