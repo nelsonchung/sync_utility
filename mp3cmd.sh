30dc10f60192ed9d2b8439a9ee55a8c87872c0b4
nelsonchung <nelson_chung@askey.com.tw>
2017-02-25 11:50:54 +0800

    增加mp3操作指令的腳本

---
+++ mp3cmd.sh
@@ -0,0 +1,24 @@
+
+echo "1. Add the information - album and songer"
+echo "2. Add cover image"
+read option
+
+case "$option" in 
+    //"")
+    //;;
+    "1")
+    echo "輸入album的名稱"
+    read album_info
+    echo "輸入songer的名稱"
+    read songer_info
+    id3v2 -A $album_info -a $songer_info "*.mp3"
+    ;;
+    "2")
+    echo "請先準備cover.jpg"
+    eyeD3 --add-image cover.jpg:FRONT_COVER "*.mp3"
+    ;;
+    *)
+    echo "Not support"
+    exit 1
+    ;;
+esac 
