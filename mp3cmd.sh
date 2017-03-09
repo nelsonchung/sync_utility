d9339241c94eaa1bc7a8f1e1267e2bf61ae05247
nelsonchung <chihchun.chung@gmail.com>
2017-03-09 13:59:02 +0800

    Modify mp3cmd.sh to make option 1 and 2 workable.

--- mp3cmd.sh
+++ mp3cmd.sh
@@ -11,11 +11,11 @@ case "$option" in
     read album_info
     echo "輸入songer的名稱"
     read songer_info
-    id3v2 -A $album_info -a $songer_info "*.mp3"
+    id3v2 -A "$album_info" -a "$songer_info" *.mp3
     ;;
     "2")
     echo "請先準備cover.jpg"
-    eyeD3 --add-image cover.jpg:FRONT_COVER "*.mp3"
+    eyeD3 --add-image cover.jpg:FRONT_COVER *.mp3
     ;;
     *)
     echo "Not support"
