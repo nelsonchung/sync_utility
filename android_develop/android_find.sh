dc0ced61b8a7c8b2faa12133975522dfaae5cb8c
nelsonchung <chihchun.chung@gmail.com>
2017-03-10 10:04:30 +0800

    Add the file name for filter - Makefile and Android.mk

--- android_develop/android_find.sh
+++ android_develop/android_find.sh
@@ -1,4 +1,4 @@
 #find . ! -path "*.svn*" -name "*.c" -o -name "*.h" -exec grep --color -n $1 {} + 
 #file_type="\(-name \"*.c\" -o \"*.h\" -o \"*.cpp\" -o \"*.java\" -o \"*.aidl\"\)"
 #find . -type f ! -path "*.svn*" $file_type -exec grep --color -n "$1" {} + 
-find . -type f ! -path "*.svn*" \( -name "*.h" -o -name "*.c" -o -name "*.aidl" -o -name "*.java" \) -exec grep --color -n "$1" {} + 
+find . -type f ! -path "*.svn*" \( -name "*.h" -o -name "*.c" -o -name "*.aidl" -o -name "*.java" -o -name "Makefile" -o -name "Android.mk" \) -exec grep --color -n "$1" {} + 
