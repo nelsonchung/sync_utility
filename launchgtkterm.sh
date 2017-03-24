da85acb24630dd7dffd795b4c22a9fe24b2786d5
nelsonchung <nelson_chung@askey.com.tw>
2017-03-24 17:01:16 +0800

    Add more gtkterm launch

--- launchgtkterm.sh
+++ launchgtkterm.sh
@@ -1,9 +1,12 @@
 echo "1. for PUMA5 realtek"
 echo "2. for PUMA6 celeno"
+echo "3. for Android debug"
 read option
 
 if [ $option == "1" ] ;then
 gtkterm -p /dev/ttyUSB0 -s 38400& 
-else
+elif [ $option == "2" ] ;then
 gtkterm -p /dev/ttyUSB0 -s 115200& 
+else
+gtkterm -p /dev/ttyUSB1 -s 115200& 
 fi
