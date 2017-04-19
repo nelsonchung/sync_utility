628ddf7715627681826606f2fd54c90fc494bc0c
nelsonchung <nelson_chung@askey.com.tw>
2017-04-19 11:07:55 +0800

    Verify make -j10 is ok

--- android_develop/daily_build.sh
+++ android_develop/daily_build.sh
@@ -2,4 +2,8 @@ sync_from_repo_server.sh
 source build/envsetup.sh
 lunch msm8953_64-userdebug
 #make clean
-make
+#make #verify ok
+#make -j2 #verify ok
+#make -j4a#verify ok
+#make -j8 #verify ok
+make -j10 
