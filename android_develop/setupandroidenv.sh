fb5f60cc17b4d1d1a5720650a43c650dc80b055a
nelsonchung <nelson_chung@askey.com.tw>
2017-04-19 14:30:10 +0800

    This file is for setuping android environment.

---
+++ android_develop/setupandroidenv.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+#based on google web site
+sudo apt-get install openjdk*
+sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip
+# based on freescale document
+sudo apt-get install uuid uuid-dev zlib1g-dev liblz-dev liblzo2-2 liblzo2-dev lzop git-core curl u-boot-tools mtd-utils android-tools-fsutils
