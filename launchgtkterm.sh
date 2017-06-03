73992ea2ebafbc56761b913d8db32bbbccbdf0e9
nelsonchung <nelson_chung@askey.com.tw>
2017-06-03 10:14:10 +0800

    用更明確的說明表達, ttyUSBx and baudrate

--- launchgtkterm.sh
+++ launchgtkterm.sh
@@ -1,12 +1,15 @@
-echo "1. for PUMA5 realtek"
-echo "2. for PUMA6 celeno"
-echo "3. for Android debug"
+echo "1. for ttyUSB0 38400"
+echo "2. for ttyUSB0 115200"
+echo "3. for ttyUSB1 38400"
+echo "4. for ttyUSB1 115200"
 read option
 
 if [ $option == "1" ] ;then
 gtkterm -p /dev/ttyUSB0 -s 38400& 
 elif [ $option == "2" ] ;then
 gtkterm -p /dev/ttyUSB0 -s 115200& 
+elif [ $option == "3" ] ;then
+gtkterm -p /dev/ttyUSB1 -s 38400&
 else
 gtkterm -p /dev/ttyUSB1 -s 115200& 
 fi
