b63dfb3fe7584129606d445aabb234711b522a6d
nelsonchung <nelson_chung@askey.com.tw>
2017-09-11 11:12:46 +0800

    update the information when run killprocess.sh error

--- killprocess.sh
+++ killprocess.sh
@@ -1,12 +1,12 @@
-#nelson_chung@nelsonchung-AskeyPC:~/Askey/tmp$ ps aux | grep logcat
+#nelson_chung@nelsonchung:~/tmp$ ps aux | grep logcat
 #nelson_+ 20220  0.0  0.0  30424  2444 pts/24   S    10:55   0:00 adb logcat -v time
 #nelson_+ 20221  0.0  0.0  30424  2536 pts/24   S    10:55   0:00 adb logcat -v time -b events
 #nelson_+ 20222  0.0  0.0  30424  2488 pts/24   S    10:55   0:00 adb logcat -v time threadtime
 #nelson_+ 20233  0.0  0.0  14564   952 pts/24   S+   10:55   0:00 grep --color=auto logcat
 
-#nelson_chung@nelsonchung-AskeyPC:~/Askey/tmp$ kill 20220
-#nelson_chung@nelsonchung-AskeyPC:~/Askey/tmp$ kill 20221
-#nelson_chung@nelsonchung-AskeyPC:~/Askey/tmp$ kill 20222
+#nelson_chung@nelsonchung:~/tmp$ kill 20220
+#nelson_chung@nelsonchung:~/tmp$ kill 20221
+#nelson_chung@nelsonchung:~/tmp$ kill 20222
 
 filename="killprocess.tmp"
 id="killprocess.id"
@@ -24,6 +24,7 @@ echo "kill process id - $processid"
 kill $processid
 done
 #We will always get error when kill last process, it doesn't matter
+#/home/nelson_chung/git/sync_utility/killprocess.sh: line 24: kill: (20849) - 沒有此一程序
 
 rm $filename
 rm $id
