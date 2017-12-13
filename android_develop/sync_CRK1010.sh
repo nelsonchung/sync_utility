3203ba9273d8da6baa39e27f1be5f34867242cea
Nelson Chung <nelson_chung@askey.com>
2017-12-13 23:56:50 +0800

    Modify the codebase from 4.14.52 to 4.1.15

--- android_develop/sync_CRK1010.sh
+++ android_develop/sync_CRK1010.sh
@@ -1,3 +1,3 @@
-repo init -u ssh://nelson_chung@gerrit-pu101.askey.com.tw:29418/manifest -m lnx-3.14.52_nxp_imx6ul_crk1010-d28.xml
+repo init -u ssh://nelson_chung@gerrit-pu101.askey.com.tw:29418/manifest -m lnx-4.1.15_nxp_imx6ul_crk1010-d28.xml
 repo sync
 
