06636dd300c0516a1d04a7194532d2c9fc22933c
nelsonchung <nelson_chung@askey.com>
2018-01-25 14:40:42 +0800

    Add the error handle.

--- linux_develop/daily_build_for_CRK1010.sh
+++ linux_develop/daily_build_for_CRK1010.sh
@@ -1,4 +1,7 @@
 source sync_CRK1010.sh 
+if [ "$?" != "0" ] ;then
+   echo "sync code is wrong, please check"
+   exit
+fi
 DISTRO=fsl-imx-fb MACHINE=crk1010 source fsl-setup-release.sh -b build-fb
-#DISTRO=fsl-imx-fb MACHINE=imx6ulevk source fsl-setup-release.sh -b build-fb
 bitbake fsl-image-gui
