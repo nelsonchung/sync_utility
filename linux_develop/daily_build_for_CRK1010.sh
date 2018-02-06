eaa4ca1a10677716f0ab89e4b68a306ba318826e
nelsonchung <nelson_chung@askey.com>
2018-02-06 10:03:10 +0800

    Add solution "repo rebase" for daily build fail

--- linux_develop/daily_build_for_CRK1010.sh
+++ linux_develop/daily_build_for_CRK1010.sh
@@ -1,7 +1,14 @@
 source sync_CRK1010.sh 
 if [ "$?" != "0" ] ;then
+   echo "sync code is wrong, launch sync_code_solution.sh"
+   source sync_code_solution.sh 
+   exit
+fi
+source sync_CRK1010.sh 
+if [ "$?" != "0" ] ;then
    echo "sync code is wrong, please check"
    exit
 fi
-DISTRO=fsl-imx-fb MACHINE=crk1010 source fsl-setup-release.sh -b build-fb
-bitbake fsl-image-gui
+DISTRO=fsl-imx-fb MACHINE=crk1010 source fsl-setup-release.sh -b build-fb 
+bitbake fsl-image-gui 
+bitbake core-image-minimal
