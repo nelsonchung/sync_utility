e507b5ca1f85dac360ac500b8c0ef39e074a0919
nelsonchung <nelson_chung@askey.com>
2018-02-12 16:36:10 +0800

    Add more error handle when build error.

--- linux_develop/daily_build_for_CRK1010.sh
+++ linux_develop/daily_build_for_CRK1010.sh
@@ -1,8 +1,8 @@
+ROOT_PATH=`pwd`
 source sync_CRK1010.sh 
 if [ "$?" != "0" ] ;then
    echo "sync code is wrong, launch sync_code_solution.sh"
    source sync_code_solution.sh 
-   exit
 fi
 source sync_CRK1010.sh 
 if [ "$?" != "0" ] ;then
@@ -11,4 +11,14 @@ if [ "$?" != "0" ] ;then
 fi
 DISTRO=fsl-imx-fb MACHINE=crk1010 source fsl-setup-release.sh -b build-fb 
 bitbake fsl-image-gui 
+if [ "$?" != "0" ] ;then
+   echo "bitbake fsl-image-gui fail, please check"
+   exit
+fi
+cd $ROOT_PATH
+DISTRO=fsl-imx-fb MACHINE=crk1010 source fsl-setup-release.sh -b build-core-image-minimal 
 bitbake core-image-minimal
+if [ "$?" != "0" ] ;then
+   echo "bitbake core-image-minimal fail, please check"
+   exit
+fi
