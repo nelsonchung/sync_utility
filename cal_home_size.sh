8f3c0248cdefb940e63fc5b780c1f3e289f409f0
Nelson Chung <nelson_chung@askey.com>
2018-04-10 11:20:41 +0800

    check the total use size in home directory

---
+++ cal_home_size.sh
@@ -0,0 +1,42 @@
+username="askey"
+echo "$username"
+sudo -S du -sch /home/$username << EOF
+12345678
+EOF
+
+username="cooper"
+echo "$username"
+sudo -S du -sch /home/$username << EOF
+12345678
+EOF
+
+username="eric"
+echo "$username"
+sudo -S du -sch /home/$username << EOF
+12345678
+EOF
+
+username="gary"
+echo "$username"
+sudo -S du -sch /home/$username << EOF
+12345678
+EOF
+
+username="leo"
+echo "$username"
+sudo -S du -sch /home/$username << EOF
+12345678
+EOF
+
+username="mike"
+echo "$username"
+sudo -S du -sch /home/$username << EOF
+12345678
+EOF
+
+username="nelson"
+echo "$username"
+sudo -S du -sch /home/$username << EOF
+12345678
+EOF
+
