820f25c3c8a215a8c44a452cc29c295baad29091
nelson chung <chihchun.chung@gmail.com>
2018-05-04 18:25:31 +0800

    Fix build CDR7010 error.

--- android_develop/daily_build_for_CDR7010_all.sh
+++ android_develop/daily_build_for_CDR7010_all.sh
@@ -1,7 +1,7 @@
 #sh sync_CDR7012.sh 
 #Fix the problem - sh: 0: Can't open sync_CDR7012.sh
 source sync_CDR7010.sh 
-cd LA.UM.5.6/LINUX/android
+cd LINUX/android
 source build/envsetup.sh
 lunch msm8953_64-userdebug
 #make clean
