9568f24b23a3cede7c5a2d458e7290cd50ca3d81
nelson chung <chihchun.chung@gmail.com>
2018-05-04 14:49:10 +0800

    Fix build script for CDR7010 and CDR7012

---
+++ android_develop/qualcomm_sbl1_image-CDR7010.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#export LM_LICENSE_FILE=/home/nelson/DS-5_linux_20121022_c75116/license5.dat
+export LM_LICENSE_FILE=/usr/local/flexlm/licenses/license.dat
+export ARMTOOLS=RVCT41
+export ARMCOMPILER6_CLANGOPT=--tool_variant=ult
+
+
+pushd boot_images/build/ms
+./build.sh TARGET_FAMILY=8953 --prod -c
+./build.sh -j1 TARGET_FAMILY=8953 --prod
+#./build.sh TARGET_FAMILY=8953 --prod #it will build fail
+popd
