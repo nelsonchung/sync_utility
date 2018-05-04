1c7a2c08e0b63155848e18cd114d3bc6c1ea73c5
Nelson <nelson_chung@askey.com.tw>
2018-05-04 14:46:22 +0800

    Fix build error for sbl1.img

--- android_develop/qualcomm_sbl1_image.sh
+++ android_develop/qualcomm_sbl1_image.sh
@@ -1,7 +1,12 @@
-export LM_LICENSE_FILE=/home/nelson/DS-5_linux_20121022_c75116/license5.dat
+#!/bin/bash
+#export LM_LICENSE_FILE=/home/nelson/DS-5_linux_20121022_c75116/license5.dat
+export LM_LICENSE_FILE=/usr/local/flexlm/licenses/license.dat
+export ARMTOOLS=RVCT41
 export ARMCOMPILER6_CLANGOPT=--tool_variant=ult
+
 
 pushd BOOT.BF.3.3/boot_images/build/ms
 ./build.sh TARGET_FAMILY=8953 --prod -c
-./build.sh TARGET_FAMILY=8953 --prod
+./build.sh -j1 TARGET_FAMILY=8953 --prod
+#./build.sh TARGET_FAMILY=8953 --prod #it will build fail
 popd
