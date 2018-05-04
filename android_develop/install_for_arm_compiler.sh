e617f23f060cddc7d1cf7175eb97d8de8857653f
nelson chung <chihchun.chung@gmail.com>
2018-05-04 18:26:33 +0800

    Add the description for install arm compiler

--- android_develop/install_for_arm_compiler.sh
+++ android_develop/install_for_arm_compiler.sh
@@ -5,7 +5,8 @@ sudo apt-get install uml-utilities
 sudo apt-get install lsb-base
 sudo apt-get install lsb-core
 sudo ln -sf /tmp /usr/tmp
-sudo mkdir -p /usr/local/flexlm/licenses
-sudo cp -af license5.dat /usr/local/flexlm/licenses/license.dat
 sudo apt-get install mnemosyne
 sudo dpkg-reconfigure dash
+#You can execute these two command under ARM compiler folder (DS-5_linux_20121022_c75116)
+sudo mkdir -p /usr/local/flexlm/licenses
+sudo cp -af license5.dat /usr/local/flexlm/licenses/license.dat
