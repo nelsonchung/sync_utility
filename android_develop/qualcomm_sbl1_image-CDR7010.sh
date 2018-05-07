9edf07495dbb456c5db362db9ea0e50892a914e5
Nelson <nelson_chung@askey.com.tw>
2018-05-07 15:09:50 +0800

    Fix build sbl error after delete setenv.sh file and add show the information of sbl1.mbn

--- android_develop/qualcomm_sbl1_image-CDR7010.sh
+++ android_develop/qualcomm_sbl1_image-CDR7010.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 export LM_LICENSE_FILE=/usr/local/flexlm/licenses/license.dat
-export ARMTOOLS=RVCT41
-export ARMCOMPILER6_CLANGOPT=--tool_variant=ult
+export ARM_COMPILER_PATH=~/DS-5_linux_20121022_c75116/bin
+export ARMROOT=~/DS-5_linux_20121022_c75116
+export ARMTOOLS=ARMCT5.01
+export ARMLIB=$ARMROOT/lib
+export ARMINCLUDE=$ARMROOT/include
+export ARMINC=$ARMINCLUDE
+export ARMBIN=$ARMROOT/bin
+export ARMPATH=$ARMBIN
 
 
 pushd boot_images/build/ms
@@ -9,3 +15,5 @@ pushd boot_images/build/ms
 ./build.sh -j1 TARGET_FAMILY=8953 --prod
 #./build.sh TARGET_FAMILY=8953 --prod #it will build fail
 popd
+
+ls -al boot_images/build/ms/bin/JAASANAZ
