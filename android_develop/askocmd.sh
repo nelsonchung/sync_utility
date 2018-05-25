6bcde6be1b387c73ead889904973c9cf3ce83dd4
nelson_chung <nelson_chung@askey.com.tw>
2018-05-25 16:50:17 +0800

    Add asko command

---
+++ android_develop/askocmd.sh
@@ -0,0 +1,14 @@
+
+echo "1.    asko sync:                "
+
+case "$option" in 
+    //"")
+    //;;
+    "1")
+        asko sync 
+    ;;
+     *)
+    echo "Not support"
+    exit 1
+    ;;
+esac 
