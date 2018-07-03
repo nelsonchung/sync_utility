57992f1211efab835e45304ffc109077af915487
nelson chung <chihchun.chung@gmail.com>
2018-07-03 19:26:37 +0800

    Update the build script

--- android_develop/daily_build_for_CDR7010_all.sh
+++ android_develop/daily_build_for_CDR7010_all.sh
@@ -1,14 +1,6 @@
-#sh sync_CDR7012.sh 
+#sh sync_CDR7012.sh
 #Fix the problem - sh: 0: Can't open sync_CDR7012.sh
-source sync_CDR7010.sh 
-cd LINUX/android
-source build/envsetup.sh
-lunch msm8953_64-userdebug
-#make clean
-#make #verify ok
-#make -j2 #verify ok
-#make -j4a#verify ok
-#make -j8 #verify ok
-make -j10 
-#make -j4 #test ok
-#make -j9 #test ok by Cooper and Leo
+source sync_CDR7010.sh
+source daily_build_for_CDR7010_android.sh
+source qualcomm_build_modem-CDR7010.sh
+source qualcomm_sbl1_image-CDR7010.sh
