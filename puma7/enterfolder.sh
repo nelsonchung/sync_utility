b6eaa34428b7670cdff80a61e485b97b2870b8d1
Nelson Chung <chihchun.chung@gmail.com>
2021-05-09 04:06:31 +0000

    add enterfolder.sh

---
+++ puma7/enterfolder.sh
@@ -0,0 +1,17 @@
+
+
+echo "Enter the folder:"
+echo "1.    GPIO CTL: "
+read option
+
+case "$option" in 
+    //"")
+    //;;
+    "1")
+        cd CBN_PUMA7_ATOM_SDK/CBN_SDK/cbn-package/cbn-apps/gpio_ctl
+    ;;
+    *)
+    echo "Not support"
+    exit 1
+    ;;
+esac 
