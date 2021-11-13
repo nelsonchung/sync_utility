54de209c77f37909cf0c86036ce41f554468b3c7
Nelson Chung <chihchun.chung@gmail.com>
2021-11-13 16:55:39 +0800

    add rootfs path

--- puma7/enterfolder.sh
+++ puma7/enterfolder.sh
@@ -2,13 +2,17 @@
 
 echo "You need to use - source enterfolder.sh to make effect immediately"
 echo "Enter the folder:"
-echo "1.    GPIO CTL: "
+echo "1.    ROOTFS: "
+echo "9.    GPIO CTL: "
 read option
 
 case "$option" in 
     //"")
     //;;
     "1")
+        cd Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/puma7_atom-rdk-linux/rdk-generic-broadband-image/1.0-r0/rootf
+    ;;
+    "9")
         cd CBN_PUMA7_ATOM_SDK/CBN_SDK/cbn-package/cbn-apps/gpio_ctl
     ;;
     *)
