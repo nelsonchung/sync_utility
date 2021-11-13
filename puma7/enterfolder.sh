81261c782ccd9dfb3958e9dfbe2bd0f15a7d3abd
Nelson Chung <chihchun.chung@gmail.com>
2021-11-13 17:15:30 +0800

    Add config setting and build folder

--- puma7/enterfolder.sh
+++ puma7/enterfolder.sh
@@ -2,18 +2,27 @@
 
 echo "You need to use - source enterfolder.sh to make effect immediately"
 echo "Enter the folder:"
-echo "1.    ROOTFS: "
+echo "1.    Config setting: "
+echo "2.    Build Folder: "
+echo "3.    ROOTFS: "
 echo "9.    GPIO CTL: "
 read option
-
+PWD=`pwd`
+       
 case "$option" in 
     //"")
     //;;
     "1")
-        PWD=`pwd`
-        echo $PWD
-        ls "$PWD/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/puma7_atom-rdk-linux/rdk-generic-broadband-image/1.0-r0/rootfs"
-        cd "$PWD/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/puma7_atom-rdk-linux/rdk-generic-broadband-image/1.0-r0/rootfs"
+    cd "$PWD/CBN_SDK/products/configs/"
+    ;;
+    "2")
+    cd "PWD/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/core2-32-rdk-linux/"
+    ;;
+    "3")
+        #PWD=`pwd`
+        #echo $PWD
+        #ls "$PWD/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/puma7_atom-rdk-linux/rdk-generic-broadband-image/1.0-r0/rootfs"
+    cd "$PWD/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/puma7_atom-rdk-linux/rdk-generic-broadband-image/1.0-r0/rootfs"
     ;;
     "9")
         cd CBN_PUMA7_ATOM_SDK/CBN_SDK/cbn-package/cbn-apps/gpio_ctl
