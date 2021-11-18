dacce9536f8f96b9a49618e608505ca71c4e9e39
Nelson Chung <chihchun.chung@gmail.com>
2021-11-18 14:53:30 +0800

    fine tune the script

--- puma7/enterfolder.sh
+++ puma7/enterfolder.sh
@@ -5,38 +5,49 @@ echo "Enter the folder:"
 echo "1.    Config setting: "
 echo "2.    Build Folder: "
 echo "3.    ROOTFS: "
-echo "4.    HW Restore: "
-echo "5.    NVRAM Storage: "
+echo "4.    Applications: "
+echo "5.    HW Restore: "
+echo "6.    NVRAM Storage: (ARM) "
+echo "7.    NVRAM Storage: (ATOM) "
 echo "9.    GPIO CTL: "
+
 read option
 PWD=`pwd`
+
+ATOM_PATH="CBN_PUMA7_ATOM_SDK"
+ARM_PATH="PUMA7-R7.0"
        
 case "$option" in 
     //"")
     //;;
     "1")
-    cd "$PWD/CBN_SDK/products/configs/"
+    cd "$ATOM_PATH/CBN_SDK/products/configs/"
     ;;
     "2")
-    cd "$PWD/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/core2-32-rdk-linux/"
+    cd "$ATOM_PATH/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/core2-32-rdk-linux/"
     ;;
     "3")
         #PWD=`pwd`
         #echo $PWD
         #ls "$PWD/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/puma7_atom-rdk-linux/rdk-generic-broadband-image/1.0-r0/rootfs"
-    cd "$PWD/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/puma7_atom-rdk-linux/rdk-generic-broadband-image/1.0-r0/rootfs"
+    cd "$ATOM_PATH/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/puma7_atom-rdk-linux/rdk-generic-broadband-image/1.0-r0/rootfs"
     ;;
     "4")
-    cd "$PWD/CBN_SDK/cbn-package/cbn-apps/cbn_hw_restore"
-    ;;
-    "4")
-    cd "$PWD/CBN_SDK/cbn-package/cbn-apps/cbn_hw_restore"
+    ;;cd "$PWD/CBN_SDK/cbn-package/cbn-apps/cbn_hw_restore"
+    cd "$ATOM_PATH/CBN_SDK/cbn-package/cbn-apps/"
     ;;
     "5")
-    cd PUMA7-R7.0/sdk/cable/docsis/src/vendor/nvram_storage/
+    ;;cd "$PWD/CBN_SDK/cbn-package/cbn-apps/cbn_hw_restore"
+    cd "$ATOM_PATH/CBN_SDK/cbn-package/cbn-apps/cbn_hw_restore"
+    ;;
+    "6")
+    cd "$ARM_PATH/sdk/cable/docsis/src/vendor/nvram_storage/"
+    ;;
+    "7")
+    cd "$ATOM_PATH/CBN_SDK/cbn-package/cbn-nvram/nvram_db/"
     ;;
     "9")
-    cd "$PWD/CBN_SDK/cbn-package/cbn-apps/gpio_ctl"
+    cd "$ATOM_PATH/CBN_SDK/cbn-package/cbn-apps/gpio_ctl"
     ;;
     *)
     echo "Not support"
