f4acc45c7a509aed7c5b101f3630ecf1d960bcf9
Nelson Chung <chihchun.chung@gmail.com>
2021-11-19 20:23:29 +0800

    Add SDK

--- puma7/enterfolder.sh
+++ puma7/enterfolder.sh
@@ -6,9 +6,13 @@ echo "1.    Config setting: "
 echo "2.    Build Folder: "
 echo "3.    ROOTFS: "
 echo "4.    Applications: "
-echo "5.    HW Restore: "
-echo "6.    NVRAM Storage: (ARM) "
-echo "7.    NVRAM Storage: (ATOM) "
+echo "41.    HW Restore: "
+echo "42.    Web GUI: "
+echo "43.    SNMP: (CCSP) "
+echo "44.    WiFi Agent: (CCSP) "
+echo "61.    NVRAM Storage: (ARM) "
+echo "62.    NVRAM Storage: (ATOM) "
+echo "71.    SDK - Common Component: (ARM) "
 echo "9.    GPIO CTL: "
 
 read option
@@ -33,18 +37,29 @@ case "$option" in
     cd "$ATOM_PATH/Intel_SDK/atom_rdkbos/build-puma7-atom/tmp/work/puma7_atom-rdk-linux/rdk-generic-broadband-image/1.0-r0/rootfs"
     ;;
     "4")
-    ;;cd "$PWD/CBN_SDK/cbn-package/cbn-apps/cbn_hw_restore"
     cd "$ATOM_PATH/CBN_SDK/cbn-package/cbn-apps/"
     ;;
-    "5")
+    "41")
     ;;cd "$PWD/CBN_SDK/cbn-package/cbn-apps/cbn_hw_restore"
     cd "$ATOM_PATH/CBN_SDK/cbn-package/cbn-apps/cbn_hw_restore"
     ;;
-    "6")
+    "42")
+    cd "$ATOM_PATH/CBN_SDK/meta-rdk-oem-cbn-puma7/meta-cbnwebui/recipes-packages/cbnwebui/"
+    ;;
+    "43")
+    cd "$ATOM_PATH/CBN_SDK/cbn-rdkb/components/opensource/ccsp/CcspSnmpPa"
+    ;;
+    "44")
+    cd "$ATOM_PATH/CBN_SDK/cbn-rdkb/components/opensource/ccsp/CcspWifiAgent"
+    ;;
+    "61")
     cd "$ARM_PATH/sdk/cable/docsis/src/vendor/nvram_storage/"
     ;;
-    "7")
+    "62")
     cd "$ATOM_PATH/CBN_SDK/cbn-package/cbn-nvram/nvram_db/"
+    ;;
+    "71")
+    cd "$ARM_PATH/PUMA7-R7.0 /sdk/common/common_components/"
     ;;
     "9")
     cd "$ATOM_PATH/CBN_SDK/cbn-package/cbn-apps/gpio_ctl"
