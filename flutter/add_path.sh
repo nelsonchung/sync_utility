f946ad2cee7b3079616a894ab4ef1700319aa13b
Nelson Chung <chihchun.chung@gmail.com>
2023-07-30 11:57:20 +0800

    Add the command for flutter project

---
+++ flutter/add_path.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#請使用source add_path.sh的方式來執行，path才會立即生效
+#Now always add current path
+PWD=`pwd`
+export PATH="${PATH}:$PWD"
+echo "export PATH=\"\$PATH:$PWD\"" >> ~/.bashrc #for Ubuntu
+echo "export PATH=\"\$PATH:$PWD\"" >> ~/.zshrc #for MacOS
+
